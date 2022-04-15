89c1c173070a152ecd18ef9dca543767afc072f5
Reginald Amedee <reginald.amedee@gmail.com>
2022-04-15 16:30:17 -0400

    used the map method to render the track component on the tracklist component

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -4,7 +4,12 @@ import './Track.css'
 class Track extends React.Component {
 
     renderAction () {
-        this.props.isRemoval === true ? <button className='Track-action'> - </button> : <button className="Track-action"> + </button>
+        // this.props.isRemoval === true ? <button className='Track-action'> - </button> : <button className="Track-action"> + </button>
+        if (this.props.isRemoval) {
+            return <button className='Track-action'>-</button>
+        } else {
+            return <button className='Track-action'>+</button>
+        }
     }
 
 
@@ -13,8 +18,8 @@ class Track extends React.Component {
         return (
             <div className="Track">
                 <div className="Track-information">
-                    <h3></h3>
-                    <p></p>
+                    <h3>Calico</h3>
+                    <p>10-4 | ShowBoxMoney Vol. 1</p>
                 </div>
                 {this.renderAction()}
             </div>
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './TrackList.css'
-
+import Track from '../Track/Track';
 class TrackList extends React.Component {
 
     render() {
         return (
-            <div className="Track">
-  <div className="Track-information">
-    <h3>Calico</h3>
-    <p>10-4 | ShoeboxMoney Vol 1</p>
-  </div>
-  <button className="Track-action"></button>
-</div>
+            <div className='TrackList'>
+                {
+                    this.props.searchResults.map(track => {
+                        return <Track track={track} key={track.id}/>
+                    })
+                }
+            </div>
         )
     }
 }
